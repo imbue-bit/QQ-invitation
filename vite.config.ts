70fd50bb98b00e5c4eff4fa7208ad0b1b9a390b3
Imbue <imbue2025@outlook.com>
2025-09-25 13:12:09 +0800

    ohno vite妈咪何意味

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
-
+import { resolve } from 'path'; 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QQ-invitation/', 
+  root: 'src',
+  base: '/QQ-invitation/',
   build: {
-    outDir: 'dist', // 输出目录
+    // 输出路径将相对于项目根目录（现在是 'src' 的上一级）
+    // 所以我们需要使用 '../dist' 来确保它在项目根目录下创建 dist
+    outDir: resolve(__dirname, 'dist'),
+    // 在清空目录时避免意外删除 'src' 之外的文件
+    emptyOutDir: true, 
   },
 });
